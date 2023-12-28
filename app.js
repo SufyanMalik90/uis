806d68a3e0641ebe84bb1fbb3a7f598a94b2e150
sufyanmalik90 <shaikh.arman1239090@gmail.com>
2023-12-29 01:07:19 +0500

    add user crud with error handling

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser')
+
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+const api = process.env.API_URL;
+const app = express();
+
+const userRouter = require('./routes/users');
+
+app.use(bodyParser.json());
+app.use(`${api}/users`, userRouter)
+
+
+mongoose.set('strictQuery', false);
+mongoose.connect(process.env.CONNECTION_STRING)
+    .then(() => {
+        console.log('Database Connected..');
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+app.listen(PORT, () => {
+
+    console.log(`Server is Running at PORT ${PORT}`);
+})
---
+++ routes/users.js
@@ -0,0 +1,93 @@
+// routes/users.js
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const User = require("../models/User");
+
+// GET route
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const { name, email, password, isAdmin } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ error: "Email already exists" });
+    }
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      name,
+      email,
+      passwordHash,
+      isAdmin: isAdmin || false,
+    });
+
+    const savedUser = await newUser.save();
+
+    res.status(201).json(savedUser);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const { name, email, password, isAdmin } = req.body;
+
+    let passwordHash;
+    if (password) {
+      passwordHash = await bcrypt.hash(password, 10);
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(userId, {
+      name,
+      email,
+      passwordHash,
+      isAdmin: isAdmin || false,
+    });
+
+    if (!updatedUser) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.json({
+      message: "User updated successfully",
+      user: updatedUser,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const userId = req.params.id;
+
+    const userToDelete = await User.findById(userId);
+    if (!userToDelete) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    
+    await User.findByIdAndDelete(userId);
+
+    res.json({ message: "User deleted successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+module.exports = router;
